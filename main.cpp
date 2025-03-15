ca2f656581d509526a8e5b0175717dd318f6ca3f
Thomas Ménès <64870819+OBud75@users.noreply.github.com>
2025-03-16 00:27:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ int main() {
     // Création d'un mot de passe et d'un utilisateur avec ID 100
     Password password("secure123", false);
     User user(100, password);
+    // Le commentaire est inutile et ajoute de la maintenance : le jour où
+    // vous voulez changer l'id il faudra penser à changer le commentaire.
 
     // Vérifie que le mot de passe est encrypté
     assert(password.isEncrypted() && "Le mot de passe n'est pas correctement encrypté.");
@@ -21,3 +23,6 @@ int main() {
     std::cout << "Tous les tests ont réussi !" << std::endl;
     return 0;
 }
+// https://youtu.be/Bf7vDBBOBUA?si=1YFmCe_XlkSWJXAk
+
+// Il y avait aussi normalement un partie d'exercice à faire en C avec sort et swap
